3786e9f35ff612b20f5f574df9cd85c7e2cbf930
Vitaliy-Gaitkulov <iri.vert33@gmail.com>
2021-01-04 17:28:24 +0200

    add JS. practic

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
 ////    JS. practice
 
+
+console.log(getMinAndMaxNumbers(4, -2, 5, 19, -130, 0, 10));
+const arr = [12, 15, 20, 25, 59, 79];
+console.log(average(arr));
+
 function getMinAndMaxNumbers() {
     const args1 = Array.from(arguments);
     const args2 = [...arguments];
@@ -26,4 +31,24 @@ function getMinAndMaxNumbers() {
       }
     }
     return max;
+  }
+
+  function average(a){
+      let averg = 0;
+      for(let i in a){
+        averg += a[i];
+      }
+      return averg / a.length;
+  }
+
+  function getRandomArbitrary() {
+    return Math.round(Math.random() * 100);
+  }
+
+  function setRandomArray(){
+      let rand = [];
+      for(let i = 0; i<10;i++){
+        rand[i] = getRandomArbitrary();
+      }
+      return rand;
   }
